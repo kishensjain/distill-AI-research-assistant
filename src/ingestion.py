8f5ee37213b72d896e98ad18ab2e675629e3d5ca
Kishen S Jain <jain.skishen@gmail.com>
2026-02-22 21:28:09 +0530

    Add load text function

--- src/ingestion.py
+++ src/ingestion.py
@@ -2,3 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 from pypdf import PdfReader
 from docx import Document
+
+def load_text(source:str) -> str:
+    """Accept either a URL or raw pasted text."""
+    if source.startswith("http://") or source.startswith("https://"):
+        print("Fetching URL...")
+        return fetch_url(source) #function to be defined soon
+    else:
+        return source
