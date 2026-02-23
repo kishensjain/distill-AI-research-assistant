5850f89bdf4983656ccdc18438d35ae14889c1e1
Kishen S Jain <jain.skishen@gmail.com>
2026-02-23 14:17:58 +0530

    Add load file function

--- src/ingestion.py
+++ src/ingestion.py
@@ -10,3 +10,11 @@ def load_text(source:str) -> str:
         return fetch_url(source) #function to be defined soon
     else:
         return source
+
+def load_file(file_path:str) -> str:
+    if file_path.endswith(".pdf"):
+        return extract_pdf(file_path) #function to be defined soon
+    elif file_path.endswith(".docx"):
+        return extract_docx(file_path) #function to be defined soon
+    else:
+        return open(file_path, "r").read()
